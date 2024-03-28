2842018dd741a4368da1b0860acae1721602cd43
Your Name <you@example.com>
2024-03-28 20:32:41 +0000

    update a user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sha256 = require("sha256");
+const { salt } = require("../secrets");
 
 router.post("/", (req, res) => {
   let { users, body, lastUserId } = req;
@@ -10,7 +11,7 @@ router.post("/", (req, res) => {
     res.send({ status: 0, reason: "Missing data" });
   }
 
-  password = sha256(password + "ch16-back-end");
+  password = sha256(password + salt);
 
   const user = users.find((user) => {
     return user.email === email && user.password === password;
@@ -25,6 +26,40 @@ router.post("/", (req, res) => {
   lastUserId.value += Math.floor(Math.random() * 9) + 1;
   req.users.push({ email, password, id: lastUserId.value });
   res.send({ status: 1, id: lastUserId.value });
+});
+
+router.patch("/:id", (req, res) => {
+  const { email, password } = req.body;
+  let { id } = req.params;
+  const { users } = req;
+
+  if (!(email || password)) {
+    res.send({ status: 1, reason: "Missing any data" });
+  }
+  id = Number(id);
+
+  if (!id || Number.isNaN(id)) {
+    res.send({ status: 0, reason: "Missing or invalid ID" });
+    return;
+  }
+
+  const indexOf = users.findIndex((user) => {
+    return user.id === id;
+  });
+
+  if (indexOf === -1) {
+    res.send({ status: 0, reason: "User not found, check the id" });
+  }
+
+  //the magic
+  if (email) {
+    users[indexOf].email = email;
+  }
+  if (password) {
+    users[indexOf].password = sha256(password + salt);
+  }
+
+  res.send({ status: 1 });
 });
 
 router.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors()); //slides in a few weeks about this
 
 //users state
 const users = [];

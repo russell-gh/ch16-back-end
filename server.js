a30248bb1a73ed027a01505aa542650874a0cee8
Your Name <you@example.com>
2024-03-28 19:09:29 +0000

    'starter'

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const app = express();
 
-/* middleware */
-//handle request to static files
-app.use(express.static("public"));
-//handle body
-app.use(express.json());
-
-app.use("/", require("./routes/simpsons"));
-app.use("/demo", require("./routes/demos"));
-
 const PORT = process.env.PORT || 6001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
